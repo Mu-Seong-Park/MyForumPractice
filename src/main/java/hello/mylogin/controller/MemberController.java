2f8c0d5bab963a33ee93fb40169d0d7a0129f24c
unknown <qkdkran123@gmail.com>
2023-11-22 11:33:09 +0900

    member Info

--- src/main/java/hello/mylogin/controller/MemberController.java
+++ src/main/java/hello/mylogin/controller/MemberController.java
@@ -11,10 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ValidationUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -63,6 +60,11 @@ public class MemberController {
         }
 
         return "redirect:/login";
+    }
+
+    @GetMapping("/info")
+    public String memberInfoForm(@RequestParam(value = "myProfile") String myProfile, Model model) {
+        return "member/memberInfoForm";
     }
 
 }
